0deb5b5a61cd2116e91fd0b95481c2d213dd3631
HiepHop <kevngn6695@gmail.com>
2024-05-01 09:50:03 -0700

    Commit more files

--- client/src/components/Heading.js
+++ client/src/components/Heading.js
@@ -1,11 +1,30 @@
 import React from "react";
 
 import { HeadingProps } from "../utils/type";
-
+import { motion } from "framer-motion";
 import "../assets/style/sass/components/heading.sass";
 
 function Heading(props) {
-  return <div>Heading</div>;
+  // Destructuring props to access className, children, and heading level props.
+  const { className, children, h1, h2, h3, h4, h5, h6 } = props;
+
+  // Creating an object with heading level props.
+  const headingLevels = { h1, h2, h3, h4, h5, h6 };
+
+  // Finding the first truthy heading level prop.
+  const level = Object.keys(headingLevels).find((key) => headingLevels[key]);
+
+  // Rendering the appropriate heading element with className and children props.
+  if (level) {
+    return React.createElement(
+      motion[level],
+      { className, ...props },
+      children,
+    );
+  }
+
+  // Returning null if no heading level prop is provided.
+  return null;
 }
 
 Heading.propTypes = HeadingProps;
